d16d61cc97d352e394429b5ba416fc65001001cc
TheNewTao <thenewtaop@gmail.com>
2015-12-20 19:56:38 -0500

    added source file

---
+++ src/me/thenewtao/core/FaSMain.java
@@ -0,0 +1,43 @@
+package me.thenewtao.core;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import me.thenewtao.events.InteractEvent;
+
+public class FaSMain extends JavaPlugin {
+	public ArrayList<Material> h = new ArrayList<>();
+
+	@Override
+	public void onEnable() {
+		Bukkit.getPluginManager().registerEvents(new InteractEvent(this), this);
+		h.add(Material.WOOD);
+		h.add(Material.LEAVES);
+		h.add(Material.WOOD_STAIRS);
+		h.add(Material.BIRCH_WOOD_STAIRS);
+		h.add(Material.JUNGLE_WOOD_STAIRS);
+		h.add(Material.SPRUCE_WOOD_STAIRS);
+		h.add(Material.DARK_OAK_STAIRS);
+		h.add(Material.SPRUCE_WOOD_STAIRS);
+		h.add(Material.COAL_BLOCK);
+		h.add(Material.BOOKSHELF);
+		h.add(Material.CHEST);
+		h.add(Material.NETHERRACK);
+		h.add(Material.WORKBENCH);
+		h.add(Material.LOG);
+		h.add(Material.FENCE);
+		h.add(Material.FENCE_GATE);
+		h.add(Material.TNT);
+		h.add(Material.WOOD_STEP);
+		File config = new File(getDataFolder(), "config.yml");
+		if (!config.exists()) {
+			getConfig().addDefault("message", "You can't lit this on fire. ");
+			getConfig().options().copyDefaults(true);
+			saveConfig();
+		}
+	}
+}
---
+++ src/me/thenewtao/events/InteractEvent.java
@@ -0,0 +1,39 @@
+package me.thenewtao.events;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import me.thenewtao.core.FaSMain;
+
+public class InteractEvent implements Listener {
+
+	private FaSMain main;
+
+	public InteractEvent(FaSMain main) {
+		this.main = main;
+	}
+
+	@EventHandler
+	public void onInteract(PlayerInteractEvent e) {
+		if (e.getItem() != null && e.getItem().getType() != Material.AIR) {
+			if (!e.getPlayer().hasPermission("flintandsteel.bypass")) {
+				if (e.getPlayer() != null && e.getItem().getType() == Material.FLINT_AND_STEEL) {
+					if (e.getClickedBlock() != null && e.getClickedBlock().getType() != Material.AIR
+							&& !main.h.contains(e.getClickedBlock().getType())) {
+						String one = ChatColor.YELLOW + "[";
+						String two = ChatColor.GOLD + "!";
+						String three = ChatColor.YELLOW + "]";
+						String four = ChatColor.RED + main.getConfig().getString("message");
+
+						e.getPlayer().sendMessage(one + two + three + four);
+						e.setCancelled(true);
+					}
+				}
+			}
+
+		}
+	}
+}
